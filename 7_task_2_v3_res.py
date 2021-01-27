7d313142c3a932da6bab730708c4cd51156857e2
ValeryMukhin <70387596+ValeryMukhin1711@users.noreply.github.com>
2021-01-27 17:30:39 +0300

    Add files via upload

---
+++ 7_task_2_v3_res.py
@@ -0,0 +1,49 @@
+"""
+2. Отсортируйте по возрастанию методом слияния одномерный вещественный массив,
+заданный случайными числами на промежутке [0; 50). Выведите на экран исходный
+и отсортированный массивы.
+
+Пример:
+Введите число элементов: 5
+Исходный - [46.11436617832828, 41.62921998361278, 18.45859540989644, 12.128870723745806, 8.025098788570562]
+Отсортированный - [8.025098788570562, 12.128870723745806, 18.45859540989644, 41.62921998361278, 46.11436617832828]
+"""
+
+
+import operator
+import random
+def merge_sort(L, compare=operator.lt):
+    if len(L) < 2:
+        return L[:]
+    else:
+        middle = int(len(L) / 2)
+        left = merge_sort(L[:middle], compare)
+        right = merge_sort(L[middle:], compare)
+        return merge(left, right, compare)
+
+def merge(left, right, compare):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if compare(left[i], right[j]):
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    while i < len(left):
+        result.append(left[i])
+        i += 1
+    while j < len(right):
+        result.append(right[j])
+        j += 1
+    return result 
+
+n = int(input("Input numbers of elements "))
+lst =[]
+for i in range(n):
+    lst.append(random.random()*50)
+print(f"\ninitial lst\n {lst}")
+print(f"\nsorted decrease lst \n{merge_sort(lst,operator.gt)}")
+print(f"\nsorted increase lst \n{merge_sort(lst)}")
+print(f"\ninitial lst \n{lst}")
